1565912edd3fcbf97f45f77c3cad5eb373b087f6
dtiendzai123 <hanknguyen2112@gmail.com>
2025-08-13 17:06:56 +0700

    ffx64_full_inject.js

---
+++ ffx64_full_inject.js
@@ -0,0 +1,97 @@
+let body = $response.body;
+
+try {
+    let json = JSON.parse(body);
+
+    // Các key với giá trị cụ thể
+    json["com.accept_devices_Key_auto.setting"] = "70-100";
+    json["com.act_ffxbase64_Key_adt_allow.list"] = "10";
+    json["com.adt_xbaseff64_Key_act.list"] = "1440";
+
+    // Các key còn lại mặc định là true
+    const keysTrue = [
+        "com.high_effect_device.target_lockffbasse64_sellect.on.allow.file",
+        "com-ve-hhighc-ffbase64-plist",
+        "com-ve-hhighc-ffbase32-plist",
+        "com-ur-settingaccpt-prefer.plist",
+        "com-ur-system -prefer.data",
+        "com.itfz-ffx32base.list",
+        "com.jsonaxbaseffx64.data",
+        "com.fullaccesspointer.dat",
+        "com.lockuni-ffx64.dat",
+        "com.rightup.setting.list",
+        "com.lockini-ffbasex64",
+        "com.inirrate-ffbasex64",
+        "com.setup-lockbase.dat",
+        "com.lockr-ffx64base",
+        "com.-bz-prefer-apn",
+        "com.setting-vx-ffbase64-zip",
+        "com.exten-ffbasex64",
+        "freebase.com.ffx64base",
+        "com.regcl-ffbasex32",
+        "com.highperformance.ffbasex64",
+        "com.khfile-ffbasex64",
+        "cwom.vr-device-delay-prefer.apn",
+        "com.uc-setting-device.plist",
+        "com-vg-hhighc-prefer-ffbasex64.zip",
+        "com.vg-lockr-ffbase64",
+        "com.vg-prefer-ffbasex64z.zip",
+        "com.av-hhighc.data",
+        "com.lockl-ffx64base",
+        "com.leftup.setting.list",
+        "com.lockinix64-ffbasex64",
+        "com.br-lockup-prefer.zip",
+        "com.bzh-lockdown-prefer.dat",
+        "com.fixrecoil-ffbase64",
+        "com.lockregcl-ffbase64.apn",
+        "com-libdispatch.dylib-n0iFEP",
+        "com.Adt-actGonffbase64.dat",
+        "com.2C6B-4048GB.setting",
+        "com.A03C-1108USJBxbase64.setting",
+        "com.lockui.systemxbase64.plist",
+        "com.infordevice.systemppi-500-on.zip",
+        "com.infordevice.systemppi-500-onxffbase64-apk",
+        "com.ppi-unlocksystem/400ppi-on-prefer.android/os",
+        "com.lockleft/ui/os-high/on.high.android",
+        "com.unlock-lockhead-on-prefer/android-os",
+        "com.flextouchdelay3d.android/os",
+        "com.syystemunlockppi400-on.ffbase64.apn",
+        "com.unlocktouchdelaymedium.on/android/os",
+        "com.profileunlocktouchdelay.on-zip",
+        "com.infordevicelockreg/high.on_sever_ DAMQ-RQMD_APN_FODER",
+        "com.ppi-unlocksystem/400ppi.on-prefer_sever_DAMQ-RQMD_EXP",
+        "com.roms-ffx64base.dat",
+        "com.ram-ffx64base.dat",
+        "com.settinguptm.list",
+        "com.gamsrollm.list",
+        "com.ss-ffx64.aps",
+        "com.writecurently-ffx64base.oss",
+        "com.ext-allyacc.fzs",
+        "com.rv-settingaccpt-prefer.ips",
+        "com.system-uv-touchdelay-0.ips",
+        "com.uu.sentov-ffbase64-prefer.list",
+        "com.uu.sentov-ffbase32-prefer.list",
+        "fae_ffxbase64_DOMAIN-FULL_hyper.roll",
+        "rog.theme_ffxbase63_HKEY-USER-ROGL-PKIN_ptspd.list",
+        "com.ext-allyacc.fzs",
+        "com.itfz-ffx32base.list",
+        "com.lockuni-ffx64.dat",
+        "com.rightup.setting.list",
+        "com.lockini-ffbasex64",
+        "com.inirrate-ffbasex64",
+        "com.exten-ffbasex64",
+        "com.highperformance.ffbasex64",
+        "com.regcl-ffbasex32",
+        "com.khfile-ffbasex64",
+        "freebase.com.ffx64base",
+        "com.setup-lockbase.dat"
+    ];
+
+    keysTrue.forEach(k => json[k] = true);
+
+    body = JSON.stringify(json);
+} catch (e) {
+    console.log("❌ Không parse được JSON, giữ nguyên.");
+}
+
+$done({ body });
